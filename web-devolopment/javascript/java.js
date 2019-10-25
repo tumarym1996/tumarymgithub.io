885ace3f37670d2c6a950a8334ba49647aa27172
tumarym1996 <57013104+tumarym1996@users.noreply.github.com>
2019-10-25 17:23:20 +0000

    Add files via upload

---
+++ web-devolopment/javascript/java.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+   	
+   
+   
+   var number =22;
+   var string
+
+
+  string ="Tumarym";
+  var floatnumber = 1.23;
+  document.write("Variable number="+number+"<br/>"+ string + floatnumber);
+
+
+   </script>
